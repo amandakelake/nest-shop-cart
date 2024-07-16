2ef81f0455ab04126493b5be2cf9cbeba4cdf69e
LGC <luoguangcong@corp.netease.com>
2024-07-16 21:40:26 +0800

    feat: update graphql type

--- src/product/dto/update-product.input.ts
+++ src/product/dto/update-product.input.ts
@@ -2,4 +2,7 @@ import { CreateProductInput } from './create-product.input';
 import { InputType, Field, Int, PartialType } from '@nestjs/graphql';
 
 @InputType()
-export class UpdateProductInput extends PartialType(CreateProductInput) {}
+export class UpdateProductInput extends PartialType(CreateProductInput) {
+  @Field(() => Int)
+  id: number;
+}
